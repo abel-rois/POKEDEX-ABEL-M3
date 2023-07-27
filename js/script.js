f2efd4dc2c7a00365e56bb853ce98f8b7e349f58
Abel Rois <daimonos.wake@gmail.com>
2023-07-26 18:09:51 -0600

    Primer commit, header, footer y sections

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const pokeCard = document.querySelector("#poke-card");
+const pokeName = document.querySelector("#poke-name");
+const pokeImgContainer = document.querySelector("#poke-img-container");
+const pokeImg = document.querySelector("#poke-img");
+const pokeNumber = document.querySelector("#poke-number");
+const pokeType = document.querySelector("#poke-type");
+const pokeStats = document.querySelector("#poke-stats");
+
+const searchPokemon = function event() {
+    event.preventDefault()
+    };
