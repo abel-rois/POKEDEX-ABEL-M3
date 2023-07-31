cde45c43d739c3fc4a3c053807dbdb742db45e8a
Abel Rois <daimonos.wake@gmail.com>
2023-07-31 17:12:02 -0600

    modificaciones en html y en funciones de js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,62 @@
 const pokeApi = "https://pokeapi.co/api/v2/pokemon/";
 const buttonSearch = document.querySelector("#search");
-const pokeCard = document.querySelector("#poke-card");
+const pokeInput = document.querySelector("#barra-busqueda");
+const titleStats = document.querySelector("#title-stats");
+const form = document.querySelector("form");
 const pokeName = document.querySelector("#poke-name");
-const pokeImgContainer = document.querySelector("#poke-img-container");
 const pokeImg = document.querySelector("#poke-img");
 const pokeNumber = document.querySelector("#poke-number");
 const pokeType = document.querySelector("#poke-type");
 const pokeStats = document.querySelector("#poke-stats");
 
 buttonSearch.addEventListener("click", insertPokemon);
+form.addEventListener("submit", insertPokemon);
 
 function insertPokemon(event) {
     event.preventDefault();
-    window.fetch(`${pokeApi}${pokeName.value.toLowerCase()}`)
+    const pokemonName = pokeInput.value.toLowerCase();
+    fetch(`${pokeApi}${pokemonName}`)
     .then(response => {
         if (response.status === 404) {
-            alert("Este Pokemón no está disponible, intenta de nuevo")
+            alert("Este Pokémon no está disponible, intenta de nuevo")
         } else {
             return response.json()
+
         }
     })
     .then(responseJSON => {
+        clearPokemonData();
+        pokeName.textContent = responseJSON.name;
+        console.log(responseJSON.moves);
+        pokeImg.src = responseJSON.sprites.front_default;
+        pokeNumber.innerHTML = `<strong>Número:</strong> ${responseJSON.id}`;
+        pokeType.innerHTML = `<strong>Tipo:</strong> ${responseJSON.types.map(type => type.type.name).join(", ")}`;
+        titleStats.innerHTML = `<strong>Estadísticas:</strong>`;
+        const statsList = responseJSON.stats.map(stat => {
+            return `<li>${stat.stat.name}: ${stat.base_stat}</li>`;
+        }).join("");
 
-        // const allItems = []
-
-        const result = []
-
-        for (let pokemonInfo in responseJSON) {
-            result.push([pokemonInfo , responseJSON[pokemonInfo]])           
-        }
-        console.log(result);
+        pokeStats.innerHTML = statsList;
     })
-
-
+    .catch(error => {
+        console.error("Error al obtener el Pokémon:", error);
+    });
 }
+
+function clearPokemonData() {
+  pokeName.textContent = "";
+  pokeImg.src = "";
+  pokeNumber.textContent = "";
+  pokeType.textContent = "";
+  pokeStats.innerHTML = "";
+}
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Error al obtener el Pokémon:", error);
+  });
+
